bb0e1e25f7c8a4f2a2aea71190e995b58ada5c32
Syuukakou <tyowukylin@yahoo.co.jp>
2022-06-24 14:20:58 +0900

    2022年6月24日14点20分

--- Src/hcluster.py
+++ Src/hcluster.py
@@ -3,11 +3,11 @@ import scipy.sparse as sps
 import sys
 import numpy as np
 from itertools import combinations
-sys.path.append(r"C:\Users\Syuukakou\PycharmProjects\hierarchical_clustering")
-from Src.functions import (extract_and_plot,
-                replace_with_dict2, check_symmetric,
-                plot_pairwise_intersection_based_matrix,
-                i_min_ab_a)
+from scipy.cluster.hierarchy import dendrogram
+import matplotlib.pyplot as plt
+from datetime import datetime
+import re
+sys.path.append("/home/syuu/Project/nict_clustering")
 
 
 def calculate_distance(csr_matrix, v0, v1, alpha, beta):
@@ -108,25 +108,29 @@ def test_clustering():
 
         if not isinstance(min_i, list) and not isinstance(min_j, list):
             c.append([min_i, min_j])
-            linkage_matrix[mat_index, :] = [c.index(min_i), I, min_dist, 2]
+            linkage_matrix[mat_index, :] = [c.index(min_i), c.index(min_j), min_dist, 2]
             cluster_dict[str([min_i, min_j])] = I
         
         if isinstance(min_i, list) and not isinstance(min_j, list):
+            i = str(min_i)
             min_i.append(min_j)
             c.append(min_i)
-            linkage_matrix[mat_index, :] = [c.index(min_j), I, min_dist, len(min_i)]
+            linkage_matrix[mat_index, :] = [c.index(min_j), cluster_dict[i], min_dist, len(min_i)]
             cluster_dict[str(min_i)] = I
 
         if not isinstance(min_i, list) and isinstance(min_j, list):
+            j = str(min_j)
             min_j.append(min_i)
             c.append(min_j)
-            linkage_matrix[mat_index, :] = [c.index(min_i), I, min_dist, len(min_j)]
+            linkage_matrix[mat_index, :] = [c.index(min_i), cluster_dict[j], min_dist, len(min_j)]
             cluster_dict[str(min_j)] = I
 
         if isinstance(min_i, list) and isinstance(min_j, list):
+            i = str(min_i)
+            j = str(min_j)
             min_i.extend(min_j)
             c.append(min_i)
-            linkage_matrix[mat_index, :] = [cluster_dict[min_i], cluster_dict[min_j], min_dist, len(min_i)]
+            linkage_matrix[mat_index, :] = [cluster_dict[i], cluster_dict[j], min_dist, len(min_i)]
             cluster_dict[str(min_i)] = I
         
         print(f"Remove {min_i}, {min_j}, {min_dist}, {I}")
@@ -142,5 +146,75 @@ def test_clustering():
     print(linkage_matrix)
     np.save("hcluster/linkage_matrix.npy", linkage_matrix)
 
+def plot_dendrogram():
+    """
+    func
+    """
+    label_file_path=r"Files\input_data\avcalss_based_formatted_label_gafgyt_coinminer_sabsik_hajime_tsunami_labels.txt"
+    labels = []
+    with open(label_file_path, "r", encoding='utf-8') as f:
+        for line in f:
+            labels.append(line.strip("\n"))
+
+    label_with_index = []
+    for l in labels:
+        l_index = labels.index(l)
+        splitted = re.split("[_ : . /]", l)
+        label_with_index.append(
+            splitted[0] + "_" + str(l_index) + "_" + splitted[-1].strip("'")
+        )
+    linkage_matrix = np.load(r"Files\input_data\linkage_matrix.npy")
+
+    fig, ax = plt.subplots(figsize=(80, 60))
+    r = dendrogram(
+        linkage_matrix,
+        labels=label_with_index,
+        leaf_font_size=5, ax=ax,
+        leaf_rotation=90,
+        show_leaf_counts=False)
+    
+    # https://stackoverflow.com/questions/68122395/how-to-color-a-dendrograms-labels-according-to-defined-groups-in-python
+    ## We get the color of leaves from the scipy dendogram docs
+    # The key is called "leaves_color_list". We iterate over the list of these colors and set colors for our leaves
+    # Please note that this parameter ("leaves_color_list") is different from the "color_list" which is the color of links
+    # (as shown in the picture)
+    # For the latest names of these parameters, please refer to scipy docs
+    # https://docs.scipy.org/doc/scipy/reference/generated/scipy.cluster.hierarchy.dendrogram.html
+    xtick_labels_text = plt.gca().get_xticklabels()
+    for leaf, leaf_color in zip(xtick_labels_text, r["leaves_color_list"]):
+        leaf.set_color(leaf_color)
+    xtick_labels = [i.get_text() for i in xtick_labels_text]
+    # plt.axhline(y=0.615, color='r', ls='-')
+
+    colormap = {
+        "gafgyt": 'red',
+        "coinminer": 'black',
+        "oinminer": 'black',
+        "sabsik": "olive",
+        "hajime": "maroon",
+        "tsunami": "blue",
+        "mirai": "orange"
+    }
+
+    for label in ax.xaxis.get_ticklabels():
+        label_text = label.get_text()
+        keys = [i for i in list(colormap.keys()) if i in label_text]
+        label_index = ax.xaxis.get_ticklabels().index(label)
+        if len(keys) > 0:
+            ax.xaxis.get_ticklabels()[label_index].set_color(colormap[keys[0]])
+        else:
+            ax.xaxis.get_ticklabels()[label_index].set_color("pink")
+
+
+    plt.tight_layout()
+    plt.title("No Truncate_mode", fontsize=50)
+    ax.tick_params(axis="y", labelsize=20)
+
+    plt.savefig("hcluster/test4.pdf", format='pdf', bbox_inches='tight')
+
+
 if __name__ == "__main__":
-    test_clustering()
+    start = datetime.now()
+    # test_clustering()
+    plot_dendrogram()
+    print(f"END: {datetime.now() - start}")
